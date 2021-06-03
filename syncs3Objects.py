f03b5647ad541bb26dc1e24891726b5c9887de61
Punit Ramaji <punitramaji@users.noreply.github.com>
2021-06-04 01:21:00 +0530

    Update syncs3Objects.py

--- syncs3Objects.py
+++ syncs3Objects.py
@@ -2,5 +2,5 @@
 
 import os
 
-sync_command = f"aws s3 sync s3://fileuploaddemobucket/blue s3://fileuploaddemobucket1/"
+sync_command = f"aws s3 sync s3://fileuploaddemobucket/blue s3://fileuploaddemobucket1"
 os.system(sync_command)
