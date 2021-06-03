d9d0dd2e18f8eee8f88a1b08dc127a47fa4f280a
Punit Ramaji <punitramaji@users.noreply.github.com>
2021-06-03 16:43:12 +0530

    Update upload_files3.py

--- upload_files3.py
+++ upload_files3.py
@@ -3,7 +3,6 @@
 import os
 import sys
 import boto3
-from django.conf import settings 
 
 path = 'public/'
 local_directory = os.path.dirname(path)
@@ -40,6 +39,6 @@ for root, dirs, files in os.walk(local_directory):
              #print "Unable to delete %s..." % s3_path
     except:
         print "Deleting %s..." % s3_path
-        client.delete_object(Bucket=settings.bucket, Key='s3_path/')
+        client.delete_object(Bucket="bucket", Key="destination")
         print "Uploading %s..." % s3_path
         client.upload_file(local_path, bucket, s3_path)
